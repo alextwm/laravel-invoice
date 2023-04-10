7a2db03195834a10553d54f37e7e1da8b761d065
Alex <alexandru@twm.ro>
2023-04-10 21:31:08 +0300

    update

--- src/Traits/InvoiceTrait.php
+++ src/Traits/InvoiceTrait.php
@@ -31,28 +31,28 @@ trait InvoiceTrait
       }
    }
 
-   public static function price_without_vat($price, $cota): float
+   public static function price_without_vat($price, $cota)
    {
       $current = ($cota / 100) * $price;
 
       return number_format($price - $current, 2, '.', '');
    }
 
-   public static function value_without_vat($qty, $price, $cota): float
+   public static function value_without_vat($qty, $price, $cota)
    {
       $value = ($cota / 100) * ($price * $qty);
 
       return number_format(($price * $qty) - $value, 2, '.', '');
    }
 
-   public static function vat_value($qty, $price, $cota): float
+   public static function vat_value($qty, $price, $cota)
    {
       $value = ($cota / 100) * $price;
 
       return number_format($value * $qty, 2, '.', '');
    }
 
-   public static function total_value($items): float
+   public static function total_value($items)
    {
       $value = 0;
 
@@ -63,7 +63,7 @@ trait InvoiceTrait
       return number_format($value, 2, '.', '');
    }
 
-   public static function total_vat($items): float
+   public static function total_vat($items)
    {
       $value = 0;
 

44b3f0721678b4b0106f28468e194591ef0eee9a
Alex <alexandru@twm.ro>
2023-04-10 22:09:38 +0300

    update

--- src/Facades/LaravelInvoice.php
+++ src/Facades/LaravelInvoice.php
@@ -4,13 +4,10 @@ namespace Twm\LaravelInvoice\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @see \Twm\LaravelInvoice\Facades\LaravelInvoice
- */
 class LaravelInvoice extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return \Twm\LaravelInvoice\LaravelInvoice::class;
+        return 'laravelinvoice'
     }
 }

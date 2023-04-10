ad39fde8617d3f933fee3838ead7ba378e851f3f
Alex <alexandru@twm.ro>
2023-04-10 21:54:09 +0300

    update

--- src/Models/Number.php
+++ src/Models/Number.php
@@ -4,6 +4,15 @@ namespace Twm\LaravelInvoice\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * Twm\LaravelInvoice\Models
+ *
+ * @property mixed $id
+ * @property int $number
+ */
+
+
 class Number extends Model
 {
    protected $guarded = ['id'];

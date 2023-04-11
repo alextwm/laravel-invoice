b5e0be554268514216a04bf2fd11d1a4d6e817f9
alextwm <alextwm@users.noreply.github.com>
2023-04-11 11:57:16 +0000

    Fix styling

--- src/Models/InvoiceLine.php
+++ src/Models/InvoiceLine.php
@@ -5,14 +5,12 @@ namespace Twm\LaravelInvoice\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
-   * Twm\LaravelInvoice\Models
-   *
-   * @property float $valoare_tva
-*/
-
+ * Twm\LaravelInvoice\Models
+ *
+ * @property float $valoare_tva
+ */
 class InvoiceLine extends Model
 {
-   
    protected $guarded = ['id'];
 
    public function invoice()

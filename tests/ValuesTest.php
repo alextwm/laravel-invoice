acebff0e24901279787c65c54513e3220278ed19
alextwm <alextwm@users.noreply.github.com>
2023-05-03 06:34:29 +0000

    Fix styling

--- tests/ValuesTest.php
+++ tests/ValuesTest.php
@@ -7,45 +7,45 @@ use Twm\LaravelInvoice\InvoiceGenerator;
 class ValuesTest extends TestCase
 {
     /** @test  */
-   public function it_can_resolve_price_without_vat()
-   {
-   //      $simulatePrice = 50;
-   //      $simulateTVA = 19;
-
-   //      $expectResult = 40.5;
-
-   //      $result = InvoiceGenerator::price_without_vat($simulatePrice, $simulateTVA);
-   //      $this->assertEquals($expectResult, $result);
-      $this->assertTrue(true);
-   }
-
-    /** @test */
-    public function it_can_resolve_value_without_vat()
+    public function it_can_resolve_price_without_vat()
     {
-      //   $simulatePrice = 50;
-      //   $simulateQuantity = 6;
-      //   $simulateTVA = 19;
+        //      $simulatePrice = 50;
+        //      $simulateTVA = 19;
 
-      //   $expectResult = 243;
+        //      $expectResult = 40.5;
 
-      //   $result = InvoiceGenerator::value_without_vat($simulateQuantity, $simulatePrice, $simulateTVA);
-
-      //   $this->assertEquals($expectResult, $result);
-      $this->assertTrue(true);
+        //      $result = InvoiceGenerator::price_without_vat($simulatePrice, $simulateTVA);
+        //      $this->assertEquals($expectResult, $result);
+        $this->assertTrue(true);
     }
 
-    /** @test */
-    public function it_can_resolve_vat_value()
-    {
-      //   $simulatePrice = 50;
-      //   $simulateQuantity = 6;
-      //   $simulateTVA = 19;
+     /** @test */
+     public function it_can_resolve_value_without_vat()
+     {
+         //   $simulatePrice = 50;
+         //   $simulateQuantity = 6;
+         //   $simulateTVA = 19;
 
-      //   $expectResult = 57;
+         //   $expectResult = 243;
 
-      //   $result = InvoiceGenerator::vat_value($simulateQuantity, $simulatePrice, $simulateTVA);
+         //   $result = InvoiceGenerator::value_without_vat($simulateQuantity, $simulatePrice, $simulateTVA);
 
-      //   $this->assertEquals($expectResult, $result);
-      $this->assertTrue(true);
-    }
+         //   $this->assertEquals($expectResult, $result);
+         $this->assertTrue(true);
+     }
+
+     /** @test */
+     public function it_can_resolve_vat_value()
+     {
+         //   $simulatePrice = 50;
+         //   $simulateQuantity = 6;
+         //   $simulateTVA = 19;
+
+         //   $expectResult = 57;
+
+         //   $result = InvoiceGenerator::vat_value($simulateQuantity, $simulatePrice, $simulateTVA);
+
+         //   $this->assertEquals($expectResult, $result);
+         $this->assertTrue(true);
+     }
 }
